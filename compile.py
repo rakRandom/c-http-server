a5a43f66b3818d401cb405adb89923e49685f018
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-08-16 21:31:17 -0300

    feat: add compile.py to automatic compilation

---
+++ compile.py
@@ -0,0 +1,21 @@
+"""
+Compile automatically the project
+"""
+
+import os
+
+
+def run():
+    executable_name: str = "server"
+    files_to_compile: list[str] = []
+
+    for dirpath, _, filenames in os.walk("."):
+        for filename in filenames:
+            if filename.endswith(".c") and dirpath.startswith(".\\src"):
+                files_to_compile.append(f"{dirpath.replace('\\', '/')}/{filename}")
+    
+    os.system(f"gcc {' '.join(files_to_compile)} -o {executable_name}")
+
+
+if __name__ == "__main__":
+    run()
